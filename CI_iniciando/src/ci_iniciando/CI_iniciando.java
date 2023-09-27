db3ed9e16fe8e4dd9d86f44d3fe6b93040a8391f
Mercedes Costabile <mercedes.costabile@gmail.com>
2023-09-26 21:18:23 -0300

    first

---
+++ CI_iniciando/src/ci_iniciando/CI_iniciando.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ci_iniciando;
+
+/**
+ *
+ * @author merce
+ */
+public class CI_iniciando {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hi Jenkins");
+    }
+    
+}
